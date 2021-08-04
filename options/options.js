4d180bc628d2d27fe5c11a5c8739da79089add2f
Crowdbotics <team@crowdbotics.com>
2021-08-04 14:53:47 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "red_mountain_29453Identifier",
+  url: "https://red_mountain_29453Identifier.botics.co",
+  api: "https://red_mountain_29453Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
